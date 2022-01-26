0604d704579f55379d50c981e0f4a393ba519d15
Igor <Igor.miramisawa@outlook.com>
2022-01-26 11:01:18 -0300

    @IgorMiramisawa [Adicionando Save e FindAll no ControllerVaga]

--- src/main/java/com/generation/diversifica/controllers/VagaController.java
+++ src/main/java/com/generation/diversifica/controllers/VagaController.java
@@ -1,15 +1,27 @@
 package com.generation.diversifica.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.generation.diversifica.models.Vaga;
 import com.generation.diversifica.repositories.VagaRepository;
+import com.generation.farmacia.models.Produto;
+
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 
 
@@ -38,9 +50,21 @@ public class VagaController {
 	 *  
 	 */
 	
-	@GetMapping("{id}")
-	public ResponseEntity<Vaga> getId(@PathVariable Long id) {
-		return repository.findById(id).map(resp -> {
+	@GetMapping
+	public ResponseEntity<List<Vaga>> getAll() {
+		List<Vaga> list = repository.findAll();
+
+		if (list.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(201).body(list);
+		}
+	}
+		
+	
+	@GetMapping("{idVaga}")
+	public ResponseEntity<Vaga> getId(@PathVariable Long idVaga) {
+		return repository.findById(idVaga).map(resp -> {
 			return ResponseEntity.status(200).body(resp);
 		}).orElseGet(() -> {
 			return ResponseEntity.status(404).build();
@@ -56,6 +80,15 @@ public class VagaController {
 			return ResponseEntity.status(200).body(list);
 		}
 	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Vaga> savePost(@Valid @RequestBody Vaga vaga) {
+		return ResponseEntity.status(201).body(repository.save(vaga));
+	}
+	
 
+	
+	
+	
 	
 }
