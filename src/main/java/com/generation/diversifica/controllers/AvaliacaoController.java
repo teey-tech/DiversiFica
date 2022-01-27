cad52cfb8ac4b2f811b4fdea0165202075d8752e
Bruno Ribeiro <85457639+BruRaptor@users.noreply.github.com>
2022-01-27 11:54:25 -0300

    @BruRaptor [Adicionando o findAll AvaliacaoController]

--- src/main/java/com/generation/diversifica/controllers/AvaliacaoController.java
+++ src/main/java/com/generation/diversifica/controllers/AvaliacaoController.java
@@ -1,17 +1,21 @@
 package com.generation.diversifica.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.generation.diversifica.models.Avaliacao;
 import com.generation.diversifica.models.Avalicao;
 
 
@@ -32,6 +36,11 @@ public class AvaliacaoController {
 	@Autowired
 	private AvaliacaoRepository repository;
 	
+	@GetMapping
+	public ResponseEntity<List<Avaliacao>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
 	@PutMapping("/update")
 	public ResponseEntity<Avaliacao> updatePost(@Valid @RequestBody Avaliacao updatePost) {
 		return repository.findById(updatePost.getIdAvaliacao()).map(record -> {
