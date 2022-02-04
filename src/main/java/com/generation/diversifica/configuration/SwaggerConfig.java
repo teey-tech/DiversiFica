9f534dcf61c913b001c56c862eda6912a2d5da3d
Igor <Igor.miramisawa@outlook.com>
2022-02-04 10:51:14 -0300

    @IgorMiramisawa [Criando Classe Swagger Atualizando Pom.xml]

---
+++ src/main/java/com/generation/diversifica/configuration/SwaggerConfig.java
@@ -0,0 +1,73 @@
+package com.generation.diversifica.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+/*
+ * Criando a Classe Config do Swagger
+ * 
+ * @Author Igor Miramisawa
+ * @Since 04/02/2022
+ * @Version 1.0
+ * 
+ */
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI springBlogPessoalOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Projeto DiversiFica")
+				.description("Projeto DiversiFica - Generation Brasil")
+				.version("v0.0.1")
+				.license(new License()
+				.name("generation.org.br")
+				.url("http://springdoc.org"))
+				.contact(new Contact()
+				.name("Grupo 05")
+				.url("https://linktr.ee/grupo05")
+				.email("diversifica@hotmail.com")))
+				.externalDocs(new ExternalDocumentation()
+				.description("Github")
+				.url("https://linktr.ee/grupo05"));
+	}
+
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+
+		return openApi -> {
+			openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations().forEach(operation -> {
+
+				ApiResponses apiResponses = operation.getResponses();
+
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse(String message) {
+
+		return new ApiResponse().description(message);
+
+	}
+
+}
