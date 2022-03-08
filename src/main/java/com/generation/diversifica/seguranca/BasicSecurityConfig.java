a56487da6692870dcf0554524281396dffc390fc
teey-tech <thiagobatistadg@hotmail.com>
2022-03-08 20:15:34 -0300

    @teey-tech [Corrigindo Erros]

--- src/main/java/com/generation/diversifica/seguranca/BasicSecurityConfig.java
+++ src/main/java/com/generation/diversifica/seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package com.generation.diversifica.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,34 +23,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Override
-	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
 		auth.inMemoryAuthentication()
-			.withUser("diversifica")
-			.password(passwordEncoder().encode("diversifica"))
-			.authorities("ROLE_ADMIN");
+				.withUser("diversifica")
+				.password(passwordEncoder().encode("diversifica"))
+				.authorities("ROLE_ADMIN");
 	}
-	
-	@Bean 
-	public PasswordEncoder passwordEncoder () {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
-	protected void configure (HttpSecurity http ) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/usuarios/logar").permitAll()
-			.antMatchers("/usuarios/cadastrar").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors()
-			.and().csrf().disable();
+				.antMatchers(HttpMethod.POST, "/usuarios/cadastrar").permitAll()
+				.antMatchers(HttpMethod.POST, "/usuarios/logar").permitAll()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and().cors()
+				.and().csrf().disable();
 	}
 
 }
